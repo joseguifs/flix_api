773561da95e87b2e369fbb26e92783970077de96
joseguifs <joseguilhermeagente007@gmail.com>
2025-02-25 07:55:02 -0300

    primeiro endpoint

--- genres/views.py
+++ genres/views.py
@@ -1,7 +1,7 @@
 from django.http import JsonResponse
 from genres.models import Genres
 
-def genres_view(request):
+def genres_view(request): # primeiro endpoint
     genres = Genres.objects.all() # retorna um queryset com todo objetos do modelo
     data = [{'id': genre.id, 'genres': genre.name} for genre in genres] # serializando meu queryset manualmente
     return JsonResponse(data, safe=False) # safe = False, indicando ao JsonResponse que deve serializar um objeto dict - list e tranforma em json
