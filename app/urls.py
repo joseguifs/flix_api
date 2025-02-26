59a5296658cbe9c05901812b0aa7a6cd91693f79
joseguifs <joseguilhermeagente007@gmail.com>
2025-02-26 08:47:26 -0300

    ajustando app

--- app/urls.py
+++ app/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from django.urls import path
-from genres.views import GenreCreatListView,genre_detail_view
+from genres.views import GenreCreatListView,GenreRetriveUpdateDestroyView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('genres/',GenreCreatListView.as_view(), name='genres_create_list_view'),
-    path('genres/<int:pk>/',genre_detail_view,name='genre-detail-view'), # o Django vai capturar o valor que aparece no lugar de <int:pk> e passá-lo como um argumento para a função genre_detail_view.
+    path('genres/<int:pk>/',GenreRetriveUpdateDestroyView.as_view(),name='genre-detail-view'), 
 ]
 
-#obs: o para detail, update e delete de um crud, devemos usar na url um identificador que seja único para cada objeto no bd ex: <int:pk>
+# o Django vai capturar o valor que aparece no lugar de <int:pk> e passá-lo como um argumento para a função genre_detail_view.
+# obs: o para detail, update e delete de um crud, devemos usar na url um identificador que seja único para cada objeto no bd ex: <int:pk>
